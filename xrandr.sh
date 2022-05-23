a58682161a8366ef7b6f1145319c8f799bce12ef
contagnas <contagnas@gmail.com>
2022-05-23 13:46:17 -0400

    xrandr script

---
+++ xrandr.sh
@@ -0,0 +1,3 @@
+xrandr --setmonitor DisplayPort-0-0 1280/297x1440/340+0+0 none
+xrandr --setmonitor DisplayPort-0-1 2560/595x1440/340+1280+0 none
+xrandr --setmonitor DisplayPort-0-2 1280/297x1440/340+3840+0 none
